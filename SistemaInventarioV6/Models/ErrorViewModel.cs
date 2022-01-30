4d983831a5fadf66ae0800a8b60c38c4ff15fa04
carlospiedrab <barce90@hotmail.com>
2022-01-30 14:26:02 -0800

    Crear Proyecto

---
+++ SistemaInventarioV6/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInventarioV6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
